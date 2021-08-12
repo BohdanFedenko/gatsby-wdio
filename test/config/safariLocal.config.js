2db03348a388945bf797edb27eea6745f3ce6253
bohfed <bohfed@qamadness.com>
2021-08-12 22:15:23 +0300

    update safari config

--- test/config/safariLocal.config.js
+++ test/config/safariLocal.config.js
@@ -2,13 +2,35 @@ const {config} = require('./wdio.conf');
 
 config.capabilities = [
     {
+        maxInstances: 1,
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            prefs: {
+                'intl.accept_languages': 'en,EN'
+            },
+
+            //Switch on/off headless mode in Chrome
+
+            args: [
+                'headless',
+                'lang=en',
+                // Use --disable-gpu to avoid an error from a missing Mesa
+                // library, as per
+                // https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+                'disable-gpu',
+                'window-size=1980,1080'
+            ],
+        },
+        acceptInsecureCerts: true
+    },
+    {
         browserName: 'safari',
         maxInstances: 1,
         "safariOptions": {
-            technologyPreview: true
+            technologyPreview: true,
+            allowAllCookies: true
         },
         port: 4445,
-        path: '/wd/hub/',
         acceptInsecureCerts: true
     }
 ];
@@ -18,7 +40,7 @@ config.specs = [
 ];
 
 config.services = [
-    'safaridriver'
+    'chromedriver'
 ];
 
 exports.config = config;
