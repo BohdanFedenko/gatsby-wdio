d79504da503d36727d9d67ed7d338137179301e5
FedenkoB <bohfed@qamadness.com>
2021-08-12 08:54:16 +0300

    Add chrome config for macOS

--- test/config/chromeMacOS.config.js
+++ test/config/chromeMacOS.config.js
@@ -2,13 +2,11 @@ const {config} = require('./wdio.conf');
 
 const drivers = {
     chrome: {version: 'latest'}, // https://chromedriver.chromium.org/
-    firefox: {version: 'latest'}, // https://github.com/mozilla/geckodriver/releases
-    chromiumedge: {version: '92.0.902.67'} // https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
 }
 
+// Win browsers capabilities
 config.capabilities = [
     //Leave the desired browsers uncommented to run them
-
     {
         maxInstances: 1,
         browserName: 'chrome',
@@ -30,31 +28,7 @@ config.capabilities = [
             ],
         },
         acceptInsecureCerts: true
-    },
-    {
-        maxInstances: 1,
-        browserName: 'firefox',
-        'moz:firefoxOptions': {
-            prefs: {
-                'intl.accept_languages': 'en,EN'
-            },
-
-            //Switch on/off headless mode in Firefox
-            args: ['-headless']
-        },
-    },
-    {
-        browserName: 'MicrosoftEdge',
-        'ms:edgeOptions': {
-            prefs: {
-                'intl.accept_languages': 'en,EN'
-            },
-
-            //Switch on/off headless mode in Edge
-            args: ['-headless']
-        },
     }
-
 ];
 
 config.specs = [
