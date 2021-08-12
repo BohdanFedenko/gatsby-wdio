9a7d7de2c996810a331ff3155850a61b9b570f1e
bohfed <bohfed@qamadness.com>
2021-08-12 23:14:30 +0300

    delete wdio-chromedriver-service

--- test/config/safariLocal.config.js
+++ test/config/safariLocal.config.js
@@ -2,28 +2,6 @@ const {config} = require('./wdio.conf');
 
 config.capabilities = [
     {
-        maxInstances: 1,
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-            prefs: {
-                'intl.accept_languages': 'en,EN'
-            },
-
-            //Switch on/off headless mode in Chrome
-
-            args: [
-                'headless',
-                'lang=en',
-                // Use --disable-gpu to avoid an error from a missing Mesa
-                // library, as per
-                // https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
-                'disable-gpu',
-                'window-size=1980,1080'
-            ],
-        },
-        acceptInsecureCerts: true
-    },
-    {
         browserName: 'safari'
     }
 ];
@@ -33,7 +11,7 @@ config.specs = [
 ];
 
 config.services = [
-    'chromedriver'
+    'safaridriver'
 ];
 
 exports.config = config;
